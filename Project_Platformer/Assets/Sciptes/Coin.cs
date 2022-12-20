8925a76cd9581239d421208e92c55b691c21e8f7
ZXCVitalina <andreyplanshetvl@gmail.com>
2022-12-20 10:56:11 +1000

    Add coin

---
+++ Project_Platformer/Assets/Sciptes/Coin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int count;
+    public AudioClip audioClip;
+
+    private GameObject player;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        audioSource = player.GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerController>().AddCoin(count);
+            audioSource.PlayOneShot(audioClip);
+            Destroy(gameObject);
+
+        }
+    }
+}
