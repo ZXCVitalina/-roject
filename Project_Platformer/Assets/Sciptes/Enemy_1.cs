39a32bd384463fadf29b63ae391aa43d40784743
ArsGrud8 <>
2022-12-20 11:05:26 +1000

    Enemy

---
+++ Project_Platformer/Assets/Sciptes/Enemy_1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy_1 : MonoBehaviour
+{
+    public float speed;
+    public Vector3[] position;
+
+    private int currentTarget;
+
+    public void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+            position[currentTarget],
+            speed);
+        if(transform.position == position[]
+            )
+    }
+}
