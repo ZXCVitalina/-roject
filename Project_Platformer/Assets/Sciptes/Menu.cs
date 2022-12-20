5080425ca9c9ebd06a276688a3884d77c1e5c150
ArsGrud8 <>
2022-12-20 10:41:13 +1000

    Play,Exit

--- Project_Platformer/Assets/Sciptes/Menu.cs
+++ Project_Platformer/Assets/Sciptes/Menu.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    public void Play
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
